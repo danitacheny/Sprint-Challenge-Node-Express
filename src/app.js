14a77d97f5b15c286142da542862964bbc0c6601
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-09 11:17:12 -0600

    uncomment lines 5 and 9 in app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-// const controller = require('controller/controller.js');
+const controller = require('../controller/controller.js');
 
 const PORT = 3000;
 
-// app.use(controller);
+app.use(controller);
 
 app.listen(PORT, console.log(`server listening on ${PORT}`));
