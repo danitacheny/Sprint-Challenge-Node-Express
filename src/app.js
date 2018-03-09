f1b1f38032e5321490f10cdc724154ae89c8d686
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-09 10:30:37 -0600

    initial commit
    - install node-fetch and express
    - app.js set up

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+// const controller = require('controller/controller.js');
+
+const PORT = 3000;
+
+// app.use(controller);
+
+app.listen(PORT, console.log(`server listening on ${PORT}`));
